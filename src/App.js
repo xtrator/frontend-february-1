f51c876f5adcd0ab9c969fd7ea92940cb276fe17
Alexander Luna <alexfox_2000@hotmail.com>
2023-02-16 18:31:58 -0500

    create gallery component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, { useEffect, useState } from "react";
+import Gallery from "./Gallery";
 
 function App() {
   const [user, setUser] = useState([]);
 
   const fetchData = () => {
-    fetch("https://backend-february-1.vercel.app/", {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-      },
-    })
+    fetch("https://backend-february-1.vercel.app/")
       .then((response) => response.json())
       .then((data) => setUser(data));
   };
@@ -23,6 +19,7 @@ function App() {
       <h1>Username:</h1>
       <p>name: {user.name}</p>
       <p>age: {user.age}</p>
+      <Gallery />
     </>
   );
 }
---
+++ src/Gallery.js
@@ -0,0 +1,52 @@
+import { getImageURL } from "./utils.js";
+
+export default function Gallery() {
+  return (
+    <div>
+      <h1>Notable Scientists</h1>
+      <Profile
+        name="Maria Skłodowska-Curie"
+        imageId="szV5sdG"
+        profession="physicist and chemist"
+        awards="(Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)"
+        discovered="polonium (element)"
+      />
+      <Profile
+        name="Katsuko Saruhashi"
+        imageId="YfeOqp2"
+        profession="geochemist"
+        awards="(Miyake Prize for geochemistry, Tanaka Prize)"
+        discovered="a method for measuring carbon dioxide in seawater"
+      />
+    </div>
+  );
+}
+
+function Profile(props) {
+  return (
+    <section className="profile">
+      <h2>{props.name}</h2>
+      <img
+        className="avatar"
+        src={getImageURL(props.imageId)}
+        alt={props.name}
+        width={70}
+        height={70}
+      />
+      <ul>
+        <li>
+          <b>Profession: </b>
+          {props.profession}
+        </li>
+        <li>
+          <b>Awards: {props.awards.split(",").length} </b>
+          {props.awards}
+        </li>
+        <li>
+          <b>Discovered: </b>
+          {props.discovered}
+        </li>
+      </ul>
+    </section>
+  );
+}
