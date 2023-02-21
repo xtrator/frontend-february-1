319c5423aab45f831016eb43e3cf5cd51a6129b1
Alexander Luna <alexfox_2000@hotmail.com>
2023-02-21 14:19:46 -0500

    add failsafe to List.js

--- src/List.js
+++ src/List.js
@@ -1,13 +1,25 @@
 import { usePeople } from "./PeopleContext";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TextInput from "./TextInput";
 
 export default function List() {
   const people = usePeople();
   const [selectedId, setSelectedId] = useState(0);
+
+  useEffect(() => {
+    if (people.length === 0) return;
+    const isSelectedIdPresent = people.some((person) => {
+      return person.id === selectedId;
+    });
+
+    if (!isSelectedIdPresent) {
+      setSelectedId(people[people.length - 1].id);
+    }
+  });
+
   return (
     <>
-      <p>People List from Context</p>
+      <p>People List</p>
       <ol>
         {people.map((person) => {
           return (
